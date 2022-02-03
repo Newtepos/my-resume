332061f7bdeb13ff3eeff5b35436c5fc41375fb0
Supakit <supakit.pnm@gmail.com>
2022-02-03 10:06:02 +0700

    Edit Layout component

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,6 +4,7 @@ import MainNavigation from "./MainNavigation";
 const Layout = (props) => {
     return <div>
         <MainNavigation></MainNavigation>
+        <MainNavigation></MainNavigation>
         <main>{props.children}</main>
     </div>
 }
