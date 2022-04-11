59457c39ab502b1f24de7439216813a1113c765f
Supakit <supakit.pnm@gmail.com>
2022-04-11 15:00:21 +0700

    Update: metatag viewport

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import "@fontsource/libre-franklin";
 export default function Home() {
   return (
     <Fragment>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       <Head>
         <title>Supakit CV</title>
         <meta name="description" content="Browse Supakit's CV" />
