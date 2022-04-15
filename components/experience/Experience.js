dd3e1bbe9ff197b41714c04706bee91c6ca167e1
Supakit <supakit.pnm@gmail.com>
2022-04-15 22:31:43 +0700

    Create: Experience Responsive

--- components/experience/Experience.js
+++ components/experience/Experience.js
@@ -1,23 +1,25 @@
 import classes from "./Experience.module.css";
-import ExperienceDetail from "./Experience-detail"
+import ExperienceDetail from "./Experience-detail";
 import ExpereiceSelect from "./Experience-select";
 import { useState } from "react";
 
 const Experience = (props) => {
+  const [experience, setExperience] = useState(2);
 
-    const [experience, setExperience] = useState(2);
-
-    const radioHandlerFunction = (value) => {
-        setExperience(value);
-    }
+  const radioHandlerFunction = (value) => {
+    setExperience(value);
+  };
 
   return (
     <section name="experience" className={classes.main_container}>
       <div className={classes.section_header}>
         <h3>Education & Experience</h3>
       </div>
-      <ExpereiceSelect radioHandler={radioHandlerFunction}/>
-      <ExperienceDetail experienceIndex={experience}/>
+      <div className={classes.experience}>
+        <h1>Experience</h1>
+      </div>
+      <ExpereiceSelect radioHandler={radioHandlerFunction} />
+      <ExperienceDetail experienceIndex={experience} />
     </section>
   );
 };
