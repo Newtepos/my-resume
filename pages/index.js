71c68e0c0f97c09bceef691f59248daee68c32bf
Newtepos <47894892+Newtepos@users.noreply.github.com>
2022-04-17 01:21:03 +0700

    Change: Meta viewport location

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ import "@fontsource/libre-franklin";
 export default function Home() {
   return (
     <Fragment>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       <Head>
         <title>Supakit CV</title>
         <meta name="description" content="Browse Supakit's CV" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       </Head>
       <Aboutme />
       <Skills />
